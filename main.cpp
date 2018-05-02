877a1ffbdc7cce344320e9918cce5d8513c7c9ff
abdiassantos <abdviana@gmail.com>
2018-05-02 16:23:38 -0300

    conlcuído primeira etapa do software de detecção de padrões para compiladores.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <string.h>
+#include <ctype.h>
 #include <locale.h>
+using namespace std;
 
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 
-	printf("Teste de digitção possuindo acentuação necessária!");
+	char* cadeia = (char*)malloc(10*sizeof(char));
+	int cont = 0;
+	char leitura[1];
 
+	std::cout << "Insira aqui sua Cadeia de Caracteres a ser Compiladas" << endl;
+	scanf("%[^\n]", cadeia);
+
+	while (cont < strlen(cadeia)) {
+
+		//Iniciar a Verificação;
+		//VERIFICA SE É LETRA PARA IDENTIFACDORES OU PALAVRAS RESERVADAS;
+		if (isalpha(cadeia[cont]) == 1 || isdigit(cadeia[cont]) == 1) {
+			printf("%c", cadeia[cont]);
+			cont++;
+			if(isalpha(cadeia[cont+1]) == 1 || cadeia[cont+1] == '_' || isdigit(cadeia[cont+1]) == 1){
+				printf("%c", cadeia[cont+1]);
+				cont++;
+			}
+			else if(isalpha(cadeia[cont+1]) == 1 && isalpha(cadeia[cont+2]) == 1){
+				printf("%c", cadeia[cont+1]);
+				printf("%c", cadeia[cont+2]);
+				cont++;
+			}
+			else if(isdigit(cadeia[cont]) == 1){
+				printf("%c", cadeia[cont]);
+				cont++;
+			}
+		}
+
+		//VERIFICA SE É CARACTERES ESPECIAIS ou SE É COMENTÁRIO;
+		if (isalnum(cadeia[cont]) == 0) {
+			if(cadeia[cont] == ';' || cadeia[cont] == ',' ||
+					cadeia[cont] == '.' || cadeia[cont] == '*' ||
+					cadeia[cont] == '(' || cadeia[cont] == ')' ||
+					cadeia[cont] == '=' || cadeia[cont] == '{' ||
+					cadeia[cont] == '}' || cadeia[cont] == '/' ||
+					cadeia[cont] == '@' || cadeia[cont] == '>' ||
+					cadeia[cont] == '<' || cadeia[cont] == '+' ||
+					cadeia[cont] == '-' || cadeia[cont] == ':'){
+
+						printf("%c", cadeia[cont]);
+						cont++;
+
+						if (cadeia[cont+1] == ';' || cadeia[cont+1] == ',' ||
+								cadeia[cont+1] == '.' || cadeia[cont+1] == '*' ||
+								cadeia[cont+1] == '(' || cadeia[cont+1] == ')' ||
+								cadeia[cont+1] == '=' || cadeia[cont+1] == '{' ||
+								cadeia[cont+1] == '}' || cadeia[cont+1] == '/' ||
+								cadeia[cont+1] == '@' || cadeia[cont+1] == '>' ||
+								cadeia[cont+1] == '<' || cadeia[cont+1] == '+' ||
+								cadeia[cont+1] == '-' || cadeia[cont+1] == ':') {
+
+							printf("%c", cadeia[cont+1]);
+							cont++;
+			}
+			else if(cadeia[cont] == '@' || cadeia[cont] == '/'){
+				if (cadeia[cont+1] == '@') {
+					while(cadeia[strlen(cadeia)] == '\0'){
+						printf("%c", cadeia[cont]);
+						cont++;
+					}
+				}
+			}
+			}
+		}
+	}
 	return 0;
 }
