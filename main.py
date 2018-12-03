101feeca0ceafbd02f74435e7e51ce23db0db010
Abdias Viana <abdviana@gmail.com>
2018-12-03 11:37:02 -0300

    Transcrevendo C/C++ to Python

---
+++ main.py
@@ -0,0 +1,37 @@
+cont = 0
+estado = 0
+
+cadeia = str(input("Insira sua cadeia de Caracteres a ser Compilada"))
+
+##AUTOMATOS NÚMEROS
+if cadeia[0].isdigit() == 1:
+    estado = 1
+
+    for x in cadeia:
+        while cont < len(cadeia):
+            if estado == 1:
+                if cadeia[cont].isdigit() == 1:
+                    estado = 2
+                    if cont == len(cadeia):
+                        print('É um dígito!')
+                elif cadeia[cont] == '-':
+                    esatdo = 1   
+
+            if estado == 2:
+                if cadeia[cont].isdigit() == 1:
+                    estado = 3
+            if estado == 3:
+                if cadeia[cont].isdigit() == ',':
+                    estado = 4
+                elif cadeia[cont].isdigit == 1:
+                    estado = 3
+            if estado == 4:
+                if cadeia[cont].isdigit() == 1:
+                    print('É um dígito!')
+                elif cadeia[cont].isdigit() == 1:
+                    estado == 4
+            cont += 1
+
+##PALAVRAS RESERVADAS
+
+    
